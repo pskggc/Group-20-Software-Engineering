1ca25bca601b81c2ab38084799672c23daa98fbb
Austinibeh <mailaustinibeh@gmail.com>
2019-04-22 22:38:03 -0500

    new web files

---
+++ webFiles/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="header.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <div class="headerDiv">
+    <span><b> RJI Project<b><span>
+    <a class="login" href="index.php">Login</a>
+    <a class="signup" href="createUser_form.php">SignUp</a>
+    </div>
+    </header>
